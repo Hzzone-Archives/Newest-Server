71c1d18fece7e54c11aa26bb2ad5ea8ff28ae998
Hzzone <Hzzone@outlook.com>
2017-07-20 16:31:25 +0800

    修复bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        enableCORS("*", "*", "*");
 
         get("/hello", (request, response) -> "Hello World");
 
@@ -30,4 +35,16 @@ public class Main {
     private static void log(String s){
         System.out.println(s);
     }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+        before(new Filter() {
+            @Override
+            public void handle(Request request, Response response) {
+                response.header("Access-Control-Allow-Origin", origin);
+                response.header("Access-Control-Request-Method", methods);
+                response.header("Access-Control-Allow-Headers", headers);
+            }
+        });
+    }
+
 }
